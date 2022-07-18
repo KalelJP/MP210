19ebef9ce10a9ca5c7b6246fcfa43895322dbeb6
Kalel <kalel.jong_poeta@i-azo.ch>
2022-07-18 14:42:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Say Hello");
+
+string input = Console.ReadLine();
+int testInt = 0;
+
+if (input == "Hello")
+{
+    Console.WriteLine("Bye!");
+    testInt = 1;
+}
